523df91f78e1e080031451deccfb1692322df780
Planeshifter <pburckhardt@outlook.com>
2016-04-16 22:54:13 -0400

    Updates

---
+++ main.js
@@ -0,0 +1,31 @@
+// MODULES //
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TeX from './../src/index.js';
+
+
+// MAIN EXAMPLE APP //
+
+class App extends React.Component {
+	render () {
+		return (
+			<div>
+				<header>
+				<h2><a href="https://github.com/cmu-isle/react-latex">React LaTeX Equations</a></h2>
+				<p>Render LaTeX equations using katex from Khan Academy</p>
+				<div id="box" style={ {backgroundColor: "lightgrey", width: 600, height: 300 } }>
+					This is an inline equation: <TeX raw="\frac{\sqrt{9}}{3 \times 2}"/>
+					Display mode is also supported:
+					<TeX raw="\int f(x^2) \; dx" displayMode={true} id={1} />
+				</div>
+				</header>
+			</div>
+		);
+	}
+}
+
+
+// RENDER APP //
+
+ReactDOM.render( <App />, document.querySelector( '#app' ) );
